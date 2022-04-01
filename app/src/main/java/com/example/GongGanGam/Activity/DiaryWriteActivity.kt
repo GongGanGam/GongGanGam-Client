9b9fadd2561e961762a09671eb4ffe8a3b728c2e
임수진 <sjoo0002@gmail.com>
2022-04-01 15:08:47 +0900

    gallery access permission done

--- app/src/main/java/com/example/GongGanGam/Activity/DiaryWriteActivity.kt
+++ app/src/main/java/com/example/GongGanGam/Activity/DiaryWriteActivity.kt
@@ -1,23 +1,33 @@
 package com.example.gonggangam.Activity
 
+import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.RoundedCorner
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.example.gonggangam.DiaryService.DiaryRetrofitInterface
 import com.example.gonggangam.R
 import com.example.gonggangam.RetrofitClient
@@ -27,20 +37,70 @@ import retrofit2.Retrofit
 
 
 class DiaryWriteActivity : AppCompatActivity() {
+    val PERMISSIONS_GALLERY_CODE = 100
+    var REQUIRED_PERMISSIONS = arrayOf<String>( Manifest.permission.READ_EXTERNAL_STORAGE)
+
     lateinit var binding: ActivityDiaryWriteBinding
+
     private lateinit var retrofit: Retrofit
 
     var isShare:Boolean = false
+    lateinit var diaryImg:String // 세영님 요청하신 diaryImg uri 저장 변수
 
     lateinit var observer: MyLifecycleObserver
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            PERMISSIONS_GALLERY_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한 허용 되었으면
+                    //이미지 첨부
+                    observer.selectImage()
+                }
+                else {
+                    // 권한 거부 되었으면
+                    Toast.makeText(this, "갤러리 접근 권한 거부됨", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun requestPermission(){
+        var permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            //설명이 필요한지
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //설명 필요 (사용자가 요청을 거부한 적이 있음)
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_GALLERY_CODE )
+            }else{
+                //설명 필요하지 않음
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSIONS_GALLERY_CODE )
+            }
+        }else{
+            //권한 허용
+            observer.selectImage()
+        }
+    }
+
     private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
         if(result.resultCode == Activity.RESULT_OK) {
 
             // 이미지 uri 넣기
-            binding.writeDiaryPhotoIv.setImageURI(result.data?.data)
-            Log.d("TAG_WRITE_RESULT", result.data?.data.toString())
+            diaryImg = result.data?.data.toString() // image uri 저장
+            Log.d("TAG_WRITE_RESULT", diaryImg)
 
-            // 이미지 띄우기
+            // Glide로 띄우기, 모서리 조정
+            Glide.with(this).load(result.data?.data)
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(10)))
+                .into(binding.writeDiaryPhotoIv)
+
+            // 이미지 visibility handling
             binding.writeDiaryPhotoIv.visibility = View.VISIBLE
             binding.writeDiaryPhotoXBtn.visibility = View.VISIBLE
         }
@@ -56,7 +116,6 @@ class DiaryWriteActivity : AppCompatActivity() {
         binding = ActivityDiaryWriteBinding.inflate(layoutInflater)
         observer = MyLifecycleObserver(this.activityResultRegistry)
         lifecycle.addObserver(observer)
-
 
         initListener()
         getImoji()
@@ -79,8 +138,9 @@ class DiaryWriteActivity : AppCompatActivity() {
 
     private fun saveDiary() {
         // 다이어리 저장 api 불러오면 됩니다!
-        // 레트로핏 통신 진행
+        // 첨부한 이미지 uri string은 diaryImg 입니다! 코드를 살펴보면 이해가실겁니다:)
 
+        // 레트로핏 통신 진행
         val diaryService = getRetrofit().create(DiaryRetrofitInterface::class.java) // 이렇게 사용하시면 됩니다.
     }
 
@@ -99,9 +159,7 @@ class DiaryWriteActivity : AppCompatActivity() {
 
         binding.writeUploadPhotoBtn.setOnClickListener {
             // 갤러리 권한 체크
-
-            //이미지 첨부
-            observer.selectImage()
+            requestPermission()
         }
 
         binding.writeDiaryPhotoXBtn.setOnClickListener{
@@ -189,6 +247,7 @@ class DiaryWriteActivity : AppCompatActivity() {
 
 
     }
+
 
     private fun hideKeyBoard() {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
