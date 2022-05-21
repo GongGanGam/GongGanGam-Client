f4782e1a7b6b520f7a61a91b6035da8e75322e4e
gimangi <khk211113@naver.com>
2022-05-21 13:31:22 +0900

    Save fcm device token

--- app/src/main/java/com/example/GongGanGam/activity/GlobalApplication.kt
+++ app/src/main/java/com/example/GongGanGam/activity/GlobalApplication.kt
@@ -1,11 +1,24 @@
 package com.example.GongGanGam.activity
 
 import android.app.Application
+import android.content.Context
 import com.example.gonggangam.R
 import com.kakao.sdk.common.KakaoSdk
 import com.navercorp.nid.NaverIdLoginSDK
 
 class GlobalApplication : Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        var instance: GlobalApplication? = null
+        fun context(): Context {
+            return instance!!.applicationContext
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         // 다른 초기화 코드들
--- app/src/main/java/com/example/GongGanGam/util/GongFirebaseMessagingService.kt
+++ app/src/main/java/com/example/GongGanGam/util/GongFirebaseMessagingService.kt
@@ -1,5 +1,6 @@
 package com.example.GongGanGam.util
 
+import com.example.GongGanGam.activity.GlobalApplication
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -7,9 +8,13 @@ class GongFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+
+        saveDeviceToken(GlobalApplication.context(), token)
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        // TODO : 메시지 수신 후 처리
     }
 }
