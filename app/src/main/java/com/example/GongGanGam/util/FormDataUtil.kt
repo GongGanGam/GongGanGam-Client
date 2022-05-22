425299ec161e6da14196ef7b6481248b52bc56c7
sujin-kk <sjoo0002@gmail.com>
2022-05-23 00:21:15 +0900

    refactoring

--- app/src/main/java/com/example/GongGanGam/util/FormDataUtil.kt
+++ app/src/main/java/com/example/GongGanGam/util/FormDataUtil.kt
@@ -21,39 +21,20 @@ import java.io.OutputStream
 
 object FormDataUtil {
 
-    fun getBody(key: String, value: Any): MultipartBody.Part {
-        return MultipartBody.Part.createFormData(key, value.toString())
-    }
-
-    fun getImageBody(key: String, file: File): MultipartBody.Part {
-        return MultipartBody.Part.createFormData(
-            name = key,
-            filename = file.name,
-            body = file.asRequestBody("image/*".toMediaType())
-        )
-    }
-
-    fun getVideoBody(key: String, file: File): MultipartBody.Part {
-        return MultipartBody.Part.createFormData(
-            name = key,
-            filename = file.name,
-            body = file.asRequestBody("video/*".toMediaType())
-        )
-    }
-
     @Nullable
     fun getRealPathFromURI(uri: Uri, context: Context): String? {
         val contentResolver: ContentResolver = context.contentResolver ?: return null
 
-
-        // 파일 경로를 만듬
+        // create file path
         val filePath: String = (context.applicationInfo.dataDir + File.separator
                 + System.currentTimeMillis())
+
         val file = File(filePath)
+
         try {
-            // 매개변수로 받은 uri 를 통해  이미지에 필요한 데이터를 불러 들인다.
+            // use contentResolver
             val inputStream = contentResolver.openInputStream(uri) ?: return null
-            // 이미지 데이터를 다시 내보내면서 file 객체에  만들었던 경로를 이용한다.
+
             val outputStream: OutputStream = FileOutputStream(file)
             val buf = ByteArray(1024)
             var len: Int
