3f3d9218162a51ca1bd0c8c2ccb33e6332de2cdc
sujin-kk <sjoo0002@gmail.com>
2022-05-22 23:57:40 +0900

    modify retrofit

--- app/src/main/java/com/example/GongGanGam/util/NetworkModule.kt
+++ app/src/main/java/com/example/GongGanGam/util/NetworkModule.kt
@@ -11,19 +11,19 @@ import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 private const val BASE_URL = "https://gonggangam.site/"
-private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
-    level = HttpLoggingInterceptor.Level.BODY
-}
+//private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+//    level = HttpLoggingInterceptor.Level.BODY
+//}
 
 var gson = GsonBuilder().setLenient().create()
 private val clientBuilder = OkHttpClient.Builder() // more detail retrofit log
-val loggingIntercepter = HttpLoggingInterceptor()
+private val loggingIntercepter = HttpLoggingInterceptor()
 
-private val okHttpClient = clientBuilder
-    .connectTimeout(1, TimeUnit.SECONDS)
-    .readTimeout(1, TimeUnit.SECONDS)
-    .writeTimeout(1, TimeUnit.SECONDS)
-    .build()
+//private val okHttpClient = clientBuilder
+//    .connectTimeout(1, TimeUnit.SECONDS)
+//    .readTimeout(1, TimeUnit.SECONDS)
+//    .writeTimeout(1, TimeUnit.SECONDS)
+//    .build()
 
 fun getRetrofit(): Retrofit {
     loggingIntercepter.level = HttpLoggingInterceptor.Level.BODY
@@ -35,15 +35,3 @@ fun getRetrofit(): Retrofit {
         .client(clientBuilder.build())
         .build()
 }
-
-//class AuthInterceptor : Interceptor {
-//    @Throws(IOException::class)
-//    override fun intercept(chain: Interceptor.Chain)
-//            : Response = with(chain) {
-//        val newRequest = request().newBuilder()
-//            .addHeader("x-access-token", getJwt(this))
-//            .build()
-//
-//        proceed(newRequest)
-//    }
-//}
