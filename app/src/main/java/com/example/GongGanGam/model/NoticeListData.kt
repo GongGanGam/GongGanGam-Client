c52c2a2706dd771fe47fcc45e892ff185552ffc1
gimangi <khk211113@naver.com>
2022-06-29 19:40:52 +0900

    fix visibility of NoticeData

--- app/src/main/java/com/example/GongGanGam/model/NoticeListData.kt
+++ app/src/main/java/com/example/GongGanGam/model/NoticeListData.kt
@@ -4,15 +4,15 @@ data class NoticeListData(
     val notices: ArrayList<NoticeData>,
     val page: Page
 ) {
-    data class NoticeData(
-        val title: String,
-        val noticeContent: String,
-        val noticeDate: String
-    )
-
     data class Page(
         val curPage: Int,
         val pageSize: Int,
         val totalPage: Int
     )
 }
+
+data class NoticeData(
+    val title: String,
+    val noticeContent: String,
+    val noticeDate: String
+)
